32c4582624d36f6e683ce93f5d51f29e9efffbcb
Jay Shenk <jay.shenk@gmail.com>
2017-09-23 15:15:17 -0400

    Associate categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:url, :title, :description)
+    params.require(:post).permit(:url, :title, :description, category_ids: [])
   end
 
   def set_post
